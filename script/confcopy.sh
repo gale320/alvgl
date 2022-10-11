22a52f88531fa9ad40678af3f3f740ef13d4ea7a
caoxiaodong <caoxiaodong@esightch.com.cn>
2022-10-11 14:23:50 +0800

    add lvgl drivers and demo

---
+++ script/confcopy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp lvgl/lv_drivers/lv_drv_conf_template.h config/lv_drv_conf.h
+cp lvgl/lv_demos/lv_demo_conf_template.h  config/lv_demo_conf.h
+cp lvgl/lvgl/lv_conf_template.h   config/lv_conf.h
+echo "copy config file finish"
